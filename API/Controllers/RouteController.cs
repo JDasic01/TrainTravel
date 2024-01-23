02ef289856df590faab4469bd9ee61e0beb4fa67
JDasic01 <jdasic1@gmail.com>
2024-01-23 20:54:24 +0100

    city upload works, routes doesnt

---
+++ API/Controllers/RouteController.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Mvc;
+using Neo4jClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RouteController : ControllerBase
+    {
+        
+        private readonly IGraphClient _client;
+
+        public RouteController(IGraphClient client)
+        {
+            _client = client;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(){
+             var Cities = await _client.Cypher.Match("(n: Route)")
+                                                   .Return(n => n.As<API.Models.Route>()).ResultsAsync;
+
+            return Ok(Cities);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id){
+                var Cities = await _client.Cypher.Match("(r:Route)")
+                                                    .Where((API.Models.Route r) => r.route_id == id)
+                                                    .Return(r => r.As<API.Models.Route>()).ResultsAsync;
+
+                return Ok(Cities.LastOrDefault());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody]API.Models.Route route){
+            await _client.Cypher.Create("(r:Route $route)")
+                                .WithParam("route", route)
+                                .ExecuteWithoutResultsAsync();
+
+                return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody]API.Models.Route route){
+                await _client.Cypher.Match("(r:Route)")
+                                    .Where((API.Models.Route r) => r.route_id == id)
+                                    .Set("r = $route")
+                                    .WithParam("route", route)
+                                    .ExecuteWithoutResultsAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id){
+                await  _client.Cypher.Match("(r:route)")
+                                    .Where((API.Models.Route r) => r.route_id == id)
+                                    .Delete("r")
+                                    .ExecuteWithoutResultsAsync();
+                return Ok();
+        }
+    }
+}
