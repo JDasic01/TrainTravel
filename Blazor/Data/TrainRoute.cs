60072d0c83d0fe816c159b530308681d5bf39460
JDasic01 <jdasic1@gmail.com>
2025-01-26 01:30:57 +0100

    API branch for digital ocean deploy

--- Blazor/Data/TrainRoute.cs
+++
@@ -1,9 +0,0 @@
-namespace Blazor.Data;
-
-public class TrainRoute
-{
-    public int route_id { get; set; }
-    public int line_id { get; set; }
-    public List<int> city_ids { get; set; }
-    public List<int> mileage { get; set; }
-}
--- Blazor/Data/TrainRouteCreate.cs
+++
@@ -1,9 +0,0 @@
-namespace Blazor.Data;
-
-public class TrainRouteCreate
-{
-    public int route_id { get; set; }
-    public int line_id { get; set; }
-    public string city_ids { get; set; }
-    public string mileage { get; set; }
-}
